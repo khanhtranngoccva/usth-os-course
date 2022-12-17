43d08d5b27bd590fa473a3e52d062b129fec6bfc
Khanh Tran <khanhtranngoccva@gmail.com>
2022-12-17 09:45:35 +0700

    misc: Three process pipe

--- EX3.cpp
+++ EX3.cpp
@@ -6,12 +6,40 @@
 #include <sys/wait.h>
 
 int main() {
+    int psToGrep[2];
+    int grepToWc[2];
+    pipe(psToGrep);
+    pipe(grepToWc);
+
     int pid1 = fork();
     int pid2 = fork();
-    int pid3 = fork();
 
-    waitpid(pid1, 0, 0);
-    waitpid(pid2, 0, 0);
-    waitpid(pid3, 0, 0);
-    std::cout << getpid() << std::endl;
+    // Process for ps.
+    if (pid1 && pid2) {
+        dup2(psToGrep[1], 1);
+        close(psToGrep[1]);
+        close(psToGrep[0]);
+        close(grepToWc[1]);
+        close(grepToWc[0]);
+        execlp("ps", "ps", "-ef", NULL);
+        return 0;
+    } else if (pid1) {
+        dup2(psToGrep[0], 0);
+        dup2(grepToWc[1], 1);
+        close(psToGrep[1]);
+        close(psToGrep[0]);
+        close(grepToWc[1]);
+        close(grepToWc[0]);
+        execlp("grep", "grep", "firefox", NULL);
+        return 0;
+    } else if (pid2) {
+        dup2(grepToWc[0], 0);
+        close(psToGrep[1]);
+        close(psToGrep[0]);
+        close(grepToWc[1]);
+        close(grepToWc[0]);
+        execlp("wc", "wc", "-l", NULL);
+        return 0;
+    }
 }
+
